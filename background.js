b11bfccab35c6d461775cd44b67d985b8c3c4250
ayush-devshark <ayushagarwal.dev@gmail.com>
2021-08-13 00:18:52 +0530

    add contextMenus

--- background.js
+++ background.js
@@ -1,5 +1,18 @@
 chrome.runtime.onInstalled.addListener(details => {
-    console.log(details);
+    chrome.contextMenus.create({
+        title: 'TV API',
+        id: 'com1',
+        contexts: ['page', 'selection'],
+    });
+    chrome.contextMenus.create({
+        title: 'TV API 1',
+        id: 'com2',
+        parentId: 'com1',
+        contexts: ['page', 'selection'],
+    });
+    chrome.contextMenus.onClicked.addListener(e => {
+        console.log(e);
+    });
 });
 
 console.log('background script running');
