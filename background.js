3e64ea59ec03da3ffc1cd14615f9cbd699efc654
ayush-devshark <ayushagarwal.dev@gmail.com>
2021-08-13 18:49:14 +0530

    add text API - speech to text functinoality

--- background.js
+++ background.js
@@ -5,15 +5,24 @@ chrome.runtime.onInstalled.addListener(details => {
         id: 'com1',
         contexts: ['page', 'selection'],
     });
+    chrome.contextMenus.create({
+        title: 'Read this text',
+        id: 'com2',
+        contexts: ['page', 'selection'],
+    });
     chrome.contextMenus.onClicked.addListener(e => {
-        fetch(`https://api.tvmaze.com/search/shows?q=${e.selectionText}`)
-            .then(res => res.json())
-            .then(data => {
-                console.log(data);
-                chrome.storage.local.set({
-                    shows: data,
+        if (e.menuItemId === 'com1') {
+            fetch(`https://api.tvmaze.com/search/shows?q=${e.selectionText}`)
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data);
+                    chrome.storage.local.set({
+                        shows: data,
+                    });
                 });
-            });
+        } else if (e.menuItemId === 'com2') {
+            chrome.tts.speak(e.selectionText, { lang: 'zh-CN', rate: 4 });
+        }
     });
 });
 
